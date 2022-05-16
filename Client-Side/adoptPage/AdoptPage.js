31425a92fae8d03db4a7143ee83412bd99d131f6
khumn010 <natasha@bbd.co.za>
2022-05-16 19:44:30 +0200

    Updated button

--- Client-Side/adoptPage/AdoptPage.js
+++ Client-Side/adoptPage/AdoptPage.js
@@ -1,7 +1,5 @@
-function toggle(elem)
-{
-    sec=elem.parentElement;
-    if(sec.style.width!='100%') sec.style.width='100%'; else sec.style.width='auto';
-    collapse=sec.getElementsByClassName("collapsible-content")[0];
-    if(collapse.style.height!='auto') collapse.style.height='auto'; else collapse.style.height='0px';
+
+function redirectToAdopt(){
+    window.location = 'Dummy.html';
 }
+//add Individual dog page file path here
