932840f2ce357a1a1cd0c46fef2ae404734cafad
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-05-16 20:48:33 +0200

    attempting to merge with individual page

--- Client-Side/carousel/carousel.js
+++ Client-Side/carousel/carousel.js
@@ -1,11 +1,35 @@
-const btnRight = document.getElementById("right-slider");
-const btnLeft = document.getElementById("left-slider");
-const dogImages = document.getElementById(["img-container"]);
+const dogImages = document.getElementsByClassName("dog-image");
+let currentImage = 1;
+selectSlides(currentImage);
 
-let currentImage = 0;
+window.onload = function () {
+  for (i = 0; i < selectorButtons.length; i++) {
+    dogImages[i].className += " fade";
+  }
+}
 
-btnRight.addEventListener('click', moveImage);
-function moveImage() {
-  currentImage += 1;
-  dogImages.style.transform = `translateX(${currentImage * -100}%)`;
+function selectSlides(n) {
+  display(currentImage = n);
+}
+
+function changeSlides(n) {
+  display(currentImage += n);
+}
+
+function display(n) {
+  let i;
+  const selectorButtons = document.getElementsByClassName("selector-buttons");
+
+  if (n > dogImages.length) { currentImage = 1 }
+  if (n < 1) { currentImage = dogImages.length }
+  for (i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.opacity = "0";
+  }
+
+  for (i = 0; i < selectorButtons.length; i++) {
+    selectorButtons[i].className = selectorButtons[i].className.replace(" selected", "");
+  }
+
+  dogImages[currentImage - 1].style.opacity = "1";
+  selectorButtons[currentImage - 1].className += " selected"
 }
--- Client/individualDog/individualDog.js
+++ Client/individualDog/individualDog.js
@@ -1,6 +1,10 @@
 const btnShowHideDetail = document.getElementById("hideShowButton");
 const extraDetails = document.querySelectorAll(".extraDetail");
 
+const dogImages = document.getElementsByClassName("dog-image");
+let currentImage = 1;
+selectSlides(currentImage);
+
 let extraDetailsActive = false;
 const detailsButtonHandler = function (e) {
   e.preventDefault();
@@ -20,3 +24,35 @@ const detailsButtonHandler = function (e) {
 };
 
 btnShowHideDetail.addEventListener("click", detailsButtonHandler);
+
+window.onload = function () {
+  for (i = 0; i < selectorButtons.length; i++) {
+    dogImages[i].className += " fade";
+  }
+}
+
+function selectSlides(n) {
+  display(currentImage = n);
+}
+
+function changeSlides(n) {
+  display(currentImage += n);
+}
+
+function display(n) {
+  let i;
+  const selectorButtons = document.getElementsByClassName("selector-buttons");
+
+  if (n > dogImages.length) { currentImage = 1 }
+  if (n < 1) { currentImage = dogImages.length }
+  for (i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.opacity = "0";
+  }
+
+  for (i = 0; i < selectorButtons.length; i++) {
+    selectorButtons[i].className = selectorButtons[i].className.replace(" selected", "");
+  }
+
+  dogImages[currentImage - 1].style.opacity = "1";
+  selectorButtons[currentImage - 1].className += " selected"
+}
