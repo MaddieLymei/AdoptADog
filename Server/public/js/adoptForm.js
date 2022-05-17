08259131b92d8610c5d6ef05f1a2e25b1218fc54
AndrewBB-BBD <andrewb@bbd.co.za>
2022-05-17 21:51:56 +0200

    fix: run adoptForm only after page is fully rendered

--- Server/public/js/adoptForm.js
+++ Server/public/js/adoptForm.js
@@ -1,41 +1,45 @@
-const openSlideoutButton = document.getElementById("openSlideoutButton");
-const closeSlideoutButton = document.getElementById("closeSlideoutButton");
-const slideout = document.getElementById("slideoutID");
-const successModal = document.getElementById("successModal");
-const failureModal = document.getElementById("failureModal");
+window.onload = () => {
+  const closeSlideoutButton = document.getElementById("closeSlideoutButton");
+  const slideout = document.getElementById("slideoutID");
+  const successModal = document.getElementById("successModal");
+  const failureModal = document.getElementById("failureModal");
+  const contactMeButton = document.getElementById("contactMeButton");
+  const openSlideoutButton = document.getElementById("openSlideoutButton");
 
-const contactMeButton = document.getElementById("contactMeButton");
+  openSlideoutButton.addEventListener("click", openSlideout);
+  closeSlideoutButton.addEventListener("click", closeSlideout);
+  // const success = true;
+  const success = false;
+  contactMeButton.addEventListener("click", (e) => openModal(e, success));
 
-const telPattern = (pattern = "[0-9]{3}-[0-9]{2}-[0-9]{3}");
-function validateForm() {}
+  const telPattern = (pattern = "[0-9]{3}-[0-9]{2}-[0-9]{3}");
+  function validateForm() {}
 
-function openSlideout(e) {
-  e.preventDefault();
-  slideout.style.borderLeft = "1px solid #056d68";
-  if (screen.width > 768) {
-    slideout.style.width = "400px";
-  } else if (screen.width <= 768 && screen.width > 500) {
-    slideout.style.width = "350px";
-  } else slideout.style.width = "100%";
-}
-
-function closeSlideout(e) {
-  e.preventDefault();
-  slideout.style.width = "0";
-  slideout.style.borderLeft = "none";
-}
-
-function openModal(e, success) {
-  e.preventDefault();
-  if (success) {
-    successModal.style.display = "block";
-  } else {
-    failureModal.style.display = "block";
+  function openSlideout(e) {
+    e.preventDefault();
+    slideout.style.borderLeft = "1px solid #056d68";
+    if (screen.width > 768) {
+      slideout.style.width = "400px";
+    } else if (screen.width <= 768 && screen.width > 500) {
+      slideout.style.width = "350px";
+    } else slideout.style.width = "100%";
   }
-}
 
-openSlideoutButton.addEventListener("click", openSlideout);
-closeSlideoutButton.addEventListener("click", closeSlideout);
+  function closeSlideout(e) {
+    e.preventDefault();
+    slideout.style.width = "0";
+    slideout.style.borderLeft = "none";
+  }
+
+  function openModal(e, success) {
+    e.preventDefault();
+    if (success) {
+      successModal.style.display = "block";
+    } else {
+      failureModal.style.display = "block";
+    }
+  }
+};
 
 // Add cross to failure modal
 // add event listener for cross
@@ -46,7 +50,3 @@ closeSlideoutButton.addEventListener("click", closeSlideout);
 // TODO: button hover EVERYWHERE
 
 // TODO: instead of "Contact me" use "Apply" for form button.
-
-// const success = true;
-const success = false;
-contactMeButton.addEventListener("click", (e) => openModal(e, success));
