44c750d1467df1830becc9bde4132de1c3853dc1
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-05-17 15:10:12 +0200

    no buttons for one image

--- Client/individualDog/individualDog.js
+++ Client/individualDog/individualDog.js
@@ -2,6 +2,7 @@ const btnShowHideDetail = document.getElementById("hideShowButton");
 const extraDetails = document.querySelectorAll(".extraDetail");
 
 const dogImages = document.getElementsByClassName("dog-image");
+const sliderButtons = document.getElementsByClassName("slider-left");
 let currentImage = 1;
 
 window.addEventListener("DOMContentLoaded", addBorder);
@@ -9,29 +10,36 @@ window.addEventListener("DOMContentLoaded", addBorder);
 window.addEventListener("resize", addBorder);
 
 function addBorder() {
-  for (i = 0; i < dogImages.length; i++) {
-    dogImages[i].style.borderTop = "";
-    dogImages[i].style.borderBottom = "";
-  }
+  if (dogImages.length != 1) {
+    for (i = 0; i < dogImages.length; i++) {
+      dogImages[i].style.borderTop = "";
+      dogImages[i].style.borderBottom = "";
+    }
 
-  let largestHeight = dogImages[0].offsetHeight;
-  for (a = 0; a < dogImages.length; a++) {
-    if (dogImages[a].offsetHeight > largestHeight) {
-      largestHeight = dogImages[a].offsetHeight;
+    let largestHeight = dogImages[0].offsetHeight;
+    for (a = 0; a < dogImages.length; a++) {
+      if (dogImages[a].offsetHeight > largestHeight) {
+        largestHeight = dogImages[a].offsetHeight;
+      }
+    }
+
+    for (f = 0; f < dogImages.length; f++) {
+      var diff = Math.abs(largestHeight - dogImages[f].offsetHeight);
+      if (diff > 20 && dogImages[f].offsetHeight != 0) {
+        border = (largestHeight - dogImages[f].offsetHeight) / 2;
+        console.log("largest:" + largestHeight);
+        console.log("height:" + dogImages[f].offsetHeight)
+        console.log("border:" + border);
+        dogImages[f].style.borderTop = border + 'px solid rgba(5, 109, 104,0.8)';
+        dogImages[f].style.borderBottom = border + 'px solid rgba(5, 109, 104,0.8)';
+      }
     }
   }
-
-  for (f = 0; f < dogImages.length; f++) {
-    var diff = Math.abs(largestHeight - dogImages[f].offsetHeight);
-    if (diff > 20 && dogImages[f].offsetHeight != 0) {
-      border = (largestHeight - dogImages[f].offsetHeight) / 2;
-      console.log("largest:" + largestHeight);
-      console.log("height:" + dogImages[f].offsetHeight)
-      console.log("border:" + border);
-      dogImages[f].style.borderTop = border + 'px solid rgba(5, 109, 104,0.8)';
-      dogImages[f].style.borderBottom = border + 'px solid rgba(5, 109, 104,0.8)';
-    }
+  else {
+    sliderButtons[0].remove();
+    sliderButtons[0].remove();
   }
+
 };
 
 slideshow = setInterval("changeSlides(1)", 10000);
