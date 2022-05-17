ece8db0209b3b98a88129c88a6f3941c2e3b3d15
AndrewBB-BBD <andrewb@bbd.co.za>
2022-05-17 20:59:25 +0200

    refactor: restructure files

--- Server/public/js/adoptForm.js
+++ Server/public/js/adoptForm.js
@@ -1,5 +1,52 @@
-const telPattern = pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}";
+const openSlideoutButton = document.getElementById("openSlideoutButton");
+const closeSlideoutButton = document.getElementById("closeSlideoutButton");
+const slideout = document.getElementById("slideoutID");
+const successModal = document.getElementById("successModal");
+const failureModal = document.getElementById("failureModal");
 
-function validateForm() {
-    
+const contactMeButton = document.getElementById("contactMeButton");
+
+const telPattern = (pattern = "[0-9]{3}-[0-9]{2}-[0-9]{3}");
+function validateForm() {}
+
+function openSlideout(e) {
+  e.preventDefault();
+  slideout.style.borderLeft = "1px solid #056d68";
+  if (screen.width > 768) {
+    slideout.style.width = "400px";
+  } else if (screen.width <= 768 && screen.width > 500) {
+    slideout.style.width = "350px";
+  } else slideout.style.width = "100%";
 }
+
+function closeSlideout(e) {
+  e.preventDefault();
+  slideout.style.width = "0";
+  slideout.style.borderLeft = "none";
+}
+
+function openModal(e, success) {
+  e.preventDefault();
+  if (success) {
+    successModal.style.display = "block";
+  } else {
+    failureModal.style.display = "block";
+  }
+}
+
+openSlideoutButton.addEventListener("click", openSlideout);
+closeSlideoutButton.addEventListener("click", closeSlideout);
+
+// Add cross to failure modal
+// add event listener for cross
+// &times;
+
+// TODO: try catch error handling  for success or failure modals
+
+// TODO: button hover EVERYWHERE
+
+// TODO: instead of "Contact me" use "Apply" for form button.
+
+// const success = true;
+const success = false;
+contactMeButton.addEventListener("click", (e) => openModal(e, success));
--- Server/public/js/individualDog.js
+++ Server/public/js/individualDog.js
@@ -1,6 +1,9 @@
 const btnShowHideDetail = document.getElementById("hideShowButton");
 const extraDetails = document.querySelectorAll(".extraDetail");
 
+// Get dog name from DB.
+// console.log("How do I take {dog_name} home?".replace("{dog_name}", "Jimbo"));
+
 let extraDetailsActive = false;
 const detailsButtonHandler = function (e) {
   e.preventDefault();
