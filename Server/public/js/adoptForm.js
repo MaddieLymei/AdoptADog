5488b3afb3d68971ead2188dcf0bb548fa1e077f
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-05-18 18:33:53 +0200

    adopt form changes

--- Server/public/js/adoptForm.js
+++ Server/public/js/adoptForm.js
@@ -1,19 +1,23 @@
 window.onload = () => {
   const closeSlideoutButton = document.getElementById("closeSlideoutButton");
+  const closeModalButtonFailure = document.getElementById("closeModalButtonFailure");
+  const closeModalButtonSuccess = document.getElementById("closeModalButtonSuccess");
   const slideout = document.getElementById("slideoutID");
   const successModal = document.getElementById("successModal");
   const failureModal = document.getElementById("failureModal");
   const contactMeButton = document.getElementById("contactMeButton");
   const openSlideoutButton = document.getElementById("openSlideoutButton");
+  const adoptForm = document.getElementsByClassName("adoptionForm")[0];
 
   openSlideoutButton.addEventListener("click", openSlideout);
-  closeSlideoutButton.addEventListener("click", closeSlideout);
-  // const success = true;
-  const success = false;
-  contactMeButton.addEventListener("click", (e) => openModal(e, success));
 
-  const telPattern = (pattern = "[0-9]{3}-[0-9]{2}-[0-9]{3}");
-  function validateForm() {}
+  closeSlideoutButton.addEventListener("click", closeSlideout);
+  closeModalButtonFailure.addEventListener("click", closeModalFailure);
+  closeModalButtonSuccess.addEventListener("click", closeModalSuccess);
+
+  // const success = true;
+  const success = true;
+  adoptForm.addEventListener("submit", (e) => openModal(e, success));
 
   function openSlideout(e) {
     e.preventDefault();
@@ -38,6 +42,14 @@ window.onload = () => {
     } else {
       failureModal.style.display = "block";
     }
+  }
+
+  function closeModalFailure() {
+    failureModal.style.display = "none";
+  }
+
+  function closeModalSuccess() {
+    successModal.style.display = "none";
   }
 };
 
