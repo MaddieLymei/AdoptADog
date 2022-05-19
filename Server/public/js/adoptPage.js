8fe40ea55cbd2fde70bd1e262fed4801a725e9cb
khumn010 <natasha@bbd.co.za>
2022-05-19 16:48:13 +0200

    Updates

--- Server/public/js/adoptPage.js
+++ Server/public/js/adoptPage.js
@@ -8,3 +8,13 @@ function filter() {
     window.location = "http://localhost:3000/adopt";
 }
 
+function getBreeds() {
+    axios({
+        method: "GET",
+        url: "http://localhost:3000/dogs/breed"
+    }).then((res) => {
+        console.log(res.data)
+    }).catch((err) => {
+        console.log(err)
+    });
+}
