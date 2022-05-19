d3e4ccd5847d5b9d2bf9de8469de1a70b0d558ea
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-05-19 17:57:28 +0200

    local host to aws

--- Server/public/js/adoptPage.js
+++ Server/public/js/adoptPage.js
@@ -1,10 +1,10 @@
 
-function redirectToAdopt(){
-    window.location = 'http://localhost:3000/dog/1';
+function redirectToAdopt() {
+  window.location = 'http://ec2-52-73-84-167.compute-1.amazonaws.com/dog/1';
 }
 //add Individual dog page file path here
 
 function filter() {
-    window.location = "http://localhost:3000/adopt";
+  window.location = "http://ec2-52-73-84-167.compute-1.amazonaws.com/adopt";
 }
 
--- Server/public/js/navbar.js
+++ Server/public/js/navbar.js
@@ -1,15 +1,15 @@
 function loadNavbar() {
   const nav = document.getElementById("navbar");
-  nav.innerHTML += 
+  nav.innerHTML +=
     `<header class='logoContainer'>
         <img class='websiteIcon' src='/images/dog.png' alt='logo'>
-        <a class='logo' href='http://localhost:3000/'>
+        <a class='logo' href='http://ec2-52-73-84-167.compute-1.amazonaws.com/'>
           AdoptADog
         </a>
       </header>
       <ul class='navbarList' id='navbarList'>
-        <li class='listItem'><a href='http://localhost:3000/' class='active'>Home</a></li>
-        <li class='listItem'><a href='http://localhost:3000/dogs'>Adoption</a></li>
+        <li class='listItem'><a href='http://ec2-52-73-84-167.compute-1.amazonaws.com/' class='active'>Home</a></li>
+        <li class='listItem'><a href='http://ec2-52-73-84-167.compute-1.amazonaws.com/dogs'>Adoption</a></li>
       </ul>
       <nav class='phoneMenu' id='toggleMenu'>
         <nav class='menuLine'></nav>
