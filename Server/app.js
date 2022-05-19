4c81e38543cdf129f00f946e0db773ac5c8b69bb
khumn010 <natasha@bbd.co.za>
2022-05-19 14:47:09 +0200

    made updates

--- Server/app.js
+++ Server/app.js
@@ -33,31 +33,7 @@ app.get('/dogs', (req, res) => {
 app.get('/dog/:id', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/individualDog.html'));
 })
-app.get("/filter", (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/adoptPage.html'))
-})
-app.get('/filter', ((req, res) => {
-    var mysql = require('mysql');
 
-    var db = mysql.createConnection({
-        host: "database-1.cluster-cdupzgygcurq.us-east-1.rds.amazonaws.com",
-        port:"3306",
-        user: 'admin',
-        password: 'AdoptADog',
-        database: 'adoptADog'
-    });
-    db.connect();
-    db.query('SELECT * from Breed', function(err, rows, fields) {
-        if (err) throw err;
-        var data=[];
-        for(i=0;i<rows.length;i++){
-            data.push(rows[i].name);
-        }
-     res.render('./',{data:data});
-    });
-
-}))
-'/views/adoptPage.html'
 // Where the routing to the other pages will happen.
 app.use('/dog', individualDogRouter);
 app.use('/dogs', allDogs);
